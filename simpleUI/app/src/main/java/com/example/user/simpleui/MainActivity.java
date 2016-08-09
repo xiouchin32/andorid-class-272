27ae9b3e28e84a6b14bd2d67d0e5d3a9978f0358
xiouchin32 <xiouchin32@gmail.com>
2016-08-09 10:23:25 +0800

    add buttom

--- simpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ simpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.user.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+
+    }
+    public void click (View view)
+    {
+        textView.setText("Hello textView");
     }
 }
